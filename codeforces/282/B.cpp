2c9af889edf281a0dd24c4cedc288fc45817d009
Saket Kandoi <saket.kandoi2003@gmail.com>
2022-07-26 15:23:00 +0530

    Add solution for problem `B - Painting Eggs` Link: https://codeforces.com/contest/282/problem/B Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/282/submission/165765783

--- codeforces/282/B.cpp
+++ codeforces/282/B.cpp
@@ -7,25 +7,28 @@ int main(){
 
     int n;
     scanf("%d",&n);
-    int ai,gi;
+    int ai[n],gi[n];
+    for(int i=0;i<n;i++)
+        scanf("%d %d",&ai[i],&gi[i]);
+
+    
     int Sag=0;//Stays between 500 and -500
+    //Assume true first
     char str[n];
     for(int i=0;i<n;i++){
-        scanf("%d %d",&ai,&gi);
-        if(Sag + ai<500){
-            Sag = Sag + ai;
+        if(Sag + ai[i]<500){
+            Sag = Sag + ai[i];
             str[i] = 'A';
         }
         else{
-            Sag = Sag - gi;
+            Sag = Sag - gi[i];
             str[i] = 'G';
         }
-    
     }
 
     if(Sag<=500 && Sag>=-500){
         for(int i=0;i<n;i++)
-            printf("%c",str[i]); //Check direct print
+            printf("%c",str[i]);
     }
     else
         printf("-1");
