74003a0eafebe156f26a4ec3cf20412dbe703a0a
Saket Kandoi <saket.kandoi2003@gmail.com>
2022-08-12 19:53:00 +0530

    Add solution for problem `A - Everyone Loves to Sleep` Link: https://codeforces.com/contest/1714/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1714/submission/167982511

---
+++ codeforces/1714/A.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    int t;
+    scanf("%d",&t);
+    int n,H,M,h,m;
+    while(t--){
+        scanf("%d",&n);
+        scanf("%d",&H);
+        scanf("%d",&M);
+        int ho,mo,diff;
+        int min =1501;
+        for(int i=0;i<n;i++){
+            scanf("%d",&h);
+            scanf("%d",&m);
+            if((h<H)||(h==H && m<M))
+                h = 24+h;
+            diff = (h-H)*60 + (m-M);
+            if(min>diff){
+                min = diff;
+                ho = h-H;
+                mo = m-M;
+                if(mo<0){
+                    ho = ho-1;
+                    mo = mo+60;
+                }
+            }
+        }
+        printf("%d ",ho);
+        printf("%d\n",mo);
+    }
+
+}
